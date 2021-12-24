21779948ed735fc2a1ee427aff75a17ba2e06b94
Juan <juanaditia94@gmail.com>
2021-12-24 23:28:48 +0700

    create game system bug fix

--- src/components/Container.js
+++ src/components/Container.js
@@ -78,7 +78,7 @@ function Container() {
   const [prev, setPrev] = useState(-1);
 
   function check(current) {
-    if (items[current].id == items[prev].id) {
+    if (items[current].id === items[prev].id) {
       items[current].stat = 'correct';
       items[prev].stat = 'correct';
       setItems([...items]);
